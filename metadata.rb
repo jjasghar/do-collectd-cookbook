a38cc6538e9a6500970bf26fadc96185ef31100b
JJ Asghar <jj@getchef.com>
2015-07-22 11:48:16 -0500

    forgot the @ for node

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'jj@chef.io'
 license          'Apache 2.0'
 description      'Installs/Configures do-collectd'
 long_description 'Installs/Configures do-collectd'
-version          '0.2.0'
+version          '0.2.1'
 
 supports 'ubuntu' , ">= 14.04"
 
